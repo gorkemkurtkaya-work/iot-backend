1e02d493ac6e618c978df1b7afd4d59097dd9bd4
GorkemKurtkaya <gorkemkurtkaya@elmaligroup.com>
2025-05-15 11:22:23 +0300

    Yorum Satırı Eklendi

--- backend/src/integrations/integrations.module.ts
+++ backend/src/integrations/integrations.module.ts
@@ -3,8 +3,8 @@ import { IntegrationsController } from './integrations.controller';
 import { IntegrationsService } from './integrations.service';
 
 
-// Projenin mimarisini göz önünde bulundurarak, entegrasyonlar modülünü oluşturmayı düşündüm ama sonra vazgeçtim.
-// Şuan Entegrasyonlar kullanılabilir durumda ama frontend'de gösterimi mevcut değil.
+// Projenin mimarisini göz önünde bulundurarak, entegrasyonlar modülünü oluşturmayı düşündüm ama sonra vazgeçtim. Çünkü zaten entegrasyonu yapıyorum sadece ek bir apiye ihtiyacım olmadığını düşündüğüm için iptal ettim.
+// Şuan Entegrasyonlar kullanılabilir durumda ama frontend'de gösterimi mevcut değil. 
 @Module({
   controllers: [IntegrationsController],
   providers: [IntegrationsService],
